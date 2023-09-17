7ef7d3ce7e66370674f66aa1630ce8d97c31113d
JoseEduardoRivera <l18450536@itz.edu.mx>
2023-09-17 17:51:53 -0600

    Configuracion del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre sobrepasa 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion requerido")]
+        [MaxLength(100, ErrorMessage = "Descripcion sobrepasa 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado requerido")]
+        public bool Estado { get; set; }
+    }
+}
